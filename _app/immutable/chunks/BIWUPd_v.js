baf280734c8cfd9dd5c8feb966043f0060701c9d
AyuBloom <85625843+AyuBloom@users.noreply.github.com>
2025-05-24 15:10:21 +0700

    Updates

---
+++ _app/immutable/chunks/BIWUPd_v.js
@@ -0,0 +1,1 @@
+import{e}from"./By3DsiHQ.js";e();
---
+++ _app/immutable/nodes/3.BvK7Z7NA.js
@@ -0,0 +1,11 @@
+import{a as t,t as a}from"../chunks/C8DeE9Zy.js";import"../chunks/BIWUPd_v.js";var n=a(`<div><h1 class="svelte-cmn6af">About</h1> <br> <h2 class="svelte-cmn6af">Few facts about me</h2> <li class="svelte-cmn6af"><strong class="svelte-cmn6af">Nationality:</strong> Vietnamese</li> <li class="svelte-cmn6af"><strong class="svelte-cmn6af">Occupation:</strong> 2nd year advanced high school student, majoring in foreign
+        linguistics</li> <li class="svelte-cmn6af"><strong class="svelte-cmn6af">Interests:</strong> Web development, editing photos & videos, rhythm games</li> <li class="svelte-cmn6af"><strong class="svelte-cmn6af">Achievements:</strong> Won several provincial English competitions, none
+        in the programming space yet <span class="svelte-cmn6af">(but I&#39;m working on it!)</span></li> <br> <h2 class="svelte-cmn6af">My (short) story</h2> <p class="svelte-cmn6af">This isn't a story, it's more like a timeline, but I think that is just because
+        I'm bad at writing stories.</p> <p class="svelte-cmn6af">I have started learning web development and programming in general since I was in
+        8th grade. Back then, I was just making some simple scripts for a small .io game
+        called <a href="https://zombs.io" target="_blank" class="svelte-cmn6af">zombs.io</a>, though I learned a
+        lot from it!</p> <p class="svelte-cmn6af">I moved on from making scripts to doing a wide variety of projects when I was in
+        1st year of high school. Being in an advanced high school is tough though, I had
+        to shift my focus away from this hobby from time to time. Nonetheless, I enjoyed
+        making things on the web (and still do!).</p> <p class="svelte-cmn6af">In 2nd year, I learned some languages other than JavaScript, like C++ and Swift
+        just to try my hands on things other than the web, some frameworks too. <span class="svelte-cmn6af">(as you can see, I made this website with SvelteKit!)</span></p></div>`);function l(s){var e=n();t(s,e)}export{l as component};
